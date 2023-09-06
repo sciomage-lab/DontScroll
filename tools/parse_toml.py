550a8aef751b3678c64bf9ce34c9a6c72e8fed74
dankernel <dkdkernel@gmail.com>
2023-09-06 20:15:35 +0900

    FIX: Docker run cleanup

--- tools/parse_toml.py
+++ tools/parse_toml.py
@@ -1,9 +1,32 @@
 import os
 import toml
 import sys
+import argparse
 
-config_path = os.path.join(os.path.expanduser("~"), ".config/dont_scroll/config.toml")
-config = toml.load(config_path)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        add_help=True,
+        description=r"""
+            Please provide SLACK_SIGNING_SECRET key through environment variable or config file
+            Config file is located in $HOMEDIR/.config/dont_scroll/config.toml
+            """,
+    )
 
-for key, value in config.items():
-    print(f"export {key.upper()}={value}")
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Config file path",
+        metavar="PATH",
+        required=True,
+        type=str,
+    )
+    args = parser.parse_args()
+
+    if os.path.exists(args.config):
+        config = toml.load(args.config)
+    else:
+        print("[fail] Not exist .toml config file")
+        exit()
+
+    for key, value in config.items():
+        print(f"export {key.upper()}={value}")
