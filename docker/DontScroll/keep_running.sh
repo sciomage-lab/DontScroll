33b8caa950d7d6f110b91bc6973e52f225b99a09
dankernel <dkdkernel@gmail.com>
2023-10-16 23:51:26 +0900

    DOCS: README for Getting Started

--- docker/DontScroll/keep_running.sh
+++ docker/DontScroll/keep_running.sh
@@ -7,7 +7,7 @@ echo $PYTHONPATH
 ./save_envs.sh
 
 mkdir models
-wget -O models/tinyllama-1.1b-1t-openorca.Q5_K_M.gguf https://huggingface.co/TheBloke/TinyLlama-1.1B-1T-OpenOrca-GGUF/resolve/main/tinyllama-1.1b-1t-openorca.Q5_K_M.gguf
+wget -q -O models/tinyllama-1.1b-1t-openorca.Q5_K_M.gguf https://huggingface.co/TheBloke/TinyLlama-1.1B-1T-OpenOrca-GGUF/resolve/main/tinyllama-1.1b-1t-openorca.Q5_K_M.gguf
 
 python dont_scroll/slack_message_fetcher.py
 
